e7f84f16aab7eb2b18c464c8f4b1901595a2e915
timcwatjames-lang <timcwatjames@gmail.com>
2026-06-12 19:14:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'jamb-v2';
+const FILES = [
+    './',
+    './index.html',
+    './style.css',
+    './index.js',
+    './data.js',
+    './manifest.json',
+    './icon.svg',
+    './ads.txt',
+    './robots.txt',
+    './sitemap.xml'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE).then((cache) => cache.addAll(FILES))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cached) => cached || fetch(e.request))
+    );
+});
